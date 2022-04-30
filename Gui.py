b05402f141072eddc6497b766e8a0fd6dcfdedd2
Pines <66344807+sounghyun890@users.noreply.github.com>
2022-04-30 11:29:58 +0900

    Add files via upload

---
+++ Gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.title("Pines")
+
+menu = Menu(root)
+
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="정보")
+
+root.config(menu=menu)
+
+root.geometry("250x100")
+root.resizable(False, False)
+
+root.mainloop()
