e5b4dfb49906c62c562edd8eb241e80f30849837
Pines <66344807+sounghyun890@users.noreply.github.com>
2022-05-01 18:11:59 +0900

    up

--- Gui.py
+++ Gui.py
@@ -1,42 +1,32 @@
 import tkinter.ttk as ttk
 from tkinter import *
+import os
 
 #이름 정의
 root = Tk()
-root.title("Pines")
+root.title("Pines 정품인증기")
 
 #버튼 프레임
 btn_frame = Frame(root)
 btn_frame.pack(fill="x")
 
 #버튼 1
-btn_1 = Button(btn_frame, padx=5, pady=5,width=10, text="Pro")
+def btn1():
+    os.system('slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX')
+    os.system('slmgr /skms kms8.msguides.com')
+    os.system('slmgr /ato')
+btn_1 = Button(btn_frame, padx=5, pady=5,width=10, command=btn1, text="Pro")
 btn_1.pack(side="left")
 
 #버튼 2
-btn_2 = Button(btn_frame, padx=5, pady=5, width=10, text="Home")
+def btn2():
+    os.system('slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99')
+    os.system('slmgr /skms kms8.msguides.com')
+    os.system('slmgr /ato')
+btn_2 = Button(btn_frame, padx=5, pady=5, width=10, command=btn2, text="Home")
 btn_2.pack(side="right")
 
-#로그 프레임
-log_frame = Frame(root)
-log_frame.pack(fill="x")
-scrollbar = Scrollbar(log_frame)
-scrollbar.pack(side="right", fill="y")
-
-#로그
-log = Listbox(log_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
-log.pack(side="left", fill="both", expand=True)
-scrollbar.config(command=log.yview)
-
-#진행상황 프레임
-frame_progress = LabelFrame(root, text="진행상황")
-frame_progress.pack(fill="x")
-
-#진행상황
-p_var = DoubleVar()
-progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
-progress_bar.pack(fill="x")
-
 #사이즈 정의
+root.geometry("300x50")
 root.resizable(False, False)
 root.mainloop()
