835c9b64fd4ac829731171755378a15e3422af38
Pines <66344807+sounghyun890@users.noreply.github.com>
2022-05-01 16:40:50 +0900

    up

--- Gui.py
+++ Gui.py
@@ -8,9 +8,11 @@ menu = Menu(root)
 menu_file = Menu(menu, tearoff=0)
 menu_file.add_command(label="정보")
 
+menu.add_cascade(label="더보기", menu=menu_file)
+
 root.config(menu=menu)
 
-root.geometry("250x100")
+root.geometry("350x200")
 root.resizable(False, False)
 
 root.mainloop()
