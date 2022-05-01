53f15afb027f7d2b25e92348560da7db464b937e
Pines <66344807+sounghyun890@users.noreply.github.com>
2022-05-01 17:38:55 +0900

    up

--- Gui.py
+++ Gui.py
@@ -1,6 +1,7 @@
 import tkinter.ttk as ttk
 from tkinter import *
 
+#이름 정의
 root = Tk()
 root.title("Pines")
 
@@ -9,11 +10,23 @@ btn_frame = Frame(root)
 btn_frame.pack(fill="x")
 
 #버튼 1
-btn_1 = Button(root, padx=5, pady=5,width=10, text="Pro")
+btn_1 = Button(btn_frame, padx=5, pady=5,width=10, text="Pro")
 btn_1.pack(side="left")
+
 #버튼 2
-btn_2 = Button(root, padx=5, pady=5, width=10, text="Home")
+btn_2 = Button(btn_frame, padx=5, pady=5, width=10, text="Home")
 btn_2.pack(side="right")
+
+#로그 프레임
+log_frame = Frame(root)
+log_frame.pack(fill="x")
+scrollbar = Scrollbar(log_frame)
+scrollbar.pack(side="right", fill="y")
+
+#로그
+log = Listbox(log_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+log.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=log.yview)
 
 #진행상황 프레임
 frame_progress = LabelFrame(root, text="진행상황")
@@ -26,5 +39,4 @@ progress_bar.pack(fill="x")
 
 #사이즈 정의
 root.resizable(False, False)
-
 root.mainloop()
